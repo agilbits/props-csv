6e78af39b9b5ffe0d28e6f688fdccadaa23f9e45
Hugo Corbucci <hugo.corbucci@gmail.com>
2011-07-23 11:31:56 -0300

    Changing the requires to have the binaries work wether the gem is installed or not. Hugo

--- lib/props-csv.rb
+++ lib/props-csv.rb
@@ -1,6 +1,6 @@
-require "props-csv/folder_scanner"
-require "props-csv/key_group_factory"
-require "props-csv/property_exporter"
+require File.expand_path("../props-csv/folder_scanner", __FILE__)
+require File.expand_path("../props-csv/key_group_factory", __FILE__)
+require File.expand_path("../props-csv/property_exporter", __FILE__)
 
 module PropsCSV
   VERSION = "0.0.1"
